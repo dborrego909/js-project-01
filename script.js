076f591aa4a1fc25a9fbeaa88ba65cf57071b65e
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-04 20:55:59 -0800

    Completed base line for game with comp generating random choices and inputed plater selection in js

--- script.js
+++ script.js
@@ -12,7 +12,36 @@ const options = ["rock", "paper", "scissors"]
 
 function getComputerChoice(){
     const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
     console.log(choice);
 }
 
-getComputerChoice();
+function checkWinner (playerSelection, computerSelection){
+    if (playerSelection == computerSelection){
+        return "Tie";
+    }   else if(
+            (playerSelection == "rock" && computerSelection == "scissors" ) ||
+            (playerSelection == "paper" && computerSelection == "rock" ) ||
+            (playerSelection == "scissors" && computerSelection == "paper") 
+    ){
+        return "Player";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+function playRound (playerSelection, computerSelection){
+    const result = checkWinner(playerSelection, computerSelection);
+    if (result == "Tie"){
+        return "You tied"
+    } else if (result == "Player"){
+        return `You Win! ${playerSelection} beat ${computerSelection}`
+    } else {
+            return `You Lose ${playerSelection} beats ${computerSelection}`
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
