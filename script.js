3efd16718f3561d82c9734ef2ffb89bd64ae2a23
Daniel Borrego <154102665+dborrego909@users.noreply.github.com>
2024-01-05 21:18:28 -0800

    Recreated array and function for computer to randomly pick a choice

--- script.js
+++ script.js
@@ -1,47 +1,16 @@
+// Define what the options are for game
+// options = rock, paper, scissors
 
-// Ask user for input from three choices
-// computer randomizes choice between 3 choices
-// if user picks  same as computer no one wins
-// if user picks rock and computer picks scissors, win else lose
-// if user picks paper and computer picks rock, win else lose
-// if user picks scissors and computer picks paper, win, else lose
+// Function to create computers choice
+// function getComputerChoice(); 
+// indexArray getComputerChoice
 
-console.log("Hello, world");
+const options = ["rock", "paper", "scissors"];
 
-const options = ["rock", "paper", "scissors"]
-
-function getComputerChoice(){
-    const choice = options[Math.floor(Math.random() * options.length)];
-    return choice;
-    console.log(choice);
+function getComputerChoice () {
+    const computerChoice = options[Math.floor(Math.random() * options.length)];
+    console.log(computerChoice);
+    return computerChoice;
 }
 
-function checkWinner (playerSelection, computerSelection){
-    if (playerSelection == computerSelection){
-        return "Tie";
-    }   else if(
-            (playerSelection == "rock" && computerSelection == "scissors" ) ||
-            (playerSelection == "paper" && computerSelection == "rock" ) ||
-            (playerSelection == "scissors" && computerSelection == "paper") 
-    ){
-        return "Player";
-    }
-    else {
-        return "Computer";
-    }
-}
-
-function playRound (playerSelection, computerSelection){
-    const result = checkWinner(playerSelection, computerSelection);
-    if (result == "Tie"){
-        return "You tied"
-    } else if (result == "Player"){
-        return `You Win! ${playerSelection} beat ${computerSelection}`
-    } else {
-            return `You Lose ${playerSelection} beats ${computerSelection}`
-    }
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection,computerSelection));
+getComputerChoice ()
